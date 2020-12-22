ce9253c3c85b3311272c9b7a5efb06fe8b21e50f
David Kim <david15730@snu.ac.kr>
2020-12-22 16:06:21 +0900

    Question 관련 Components

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+import react from 'react'
+
+//The Main Page needs many Components : TopBar, List of Questions, ...etc
+const Main = () => {
+    //TODO: use Components
+}
+
+export default Main
---
+++ src/components/Questions/AuthorProfile.js
@@ -0,0 +1,23 @@
+import {useState, Fragment} from 'react'
+import {getUser} from '../../axios.ts'
+
+const AuthorProfile = (id, createed_date) => {
+    const [Author, setAuthor] = useState()
+    setAuthor(getUser(id))
+    return (
+        <Fragment /*asked at: should we change this to create {date} at {time}? */>
+            asked at {createed_date}
+            <div className="author-profile-bottom">
+                <div className="author-pic">
+                    <img src={Author.picture} alt="Author's profile"></img>
+                </div>
+                <div className="author-info">
+                    <div className="author-username">{Author.username}</div>
+                    <div className="author-reputation">reputation: {Author.reputation}</div>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default AuthorProfile
