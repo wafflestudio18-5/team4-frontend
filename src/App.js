c9d6806b678f47737124e13a6c4216b7a0f5adcc
gitiss <36957738+gitiss@users.noreply.github.com>
2020-12-27 05:27:31 +0900

    update header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+<<<<<<< Updated upstream
+=======
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import History from './components/User/History'
+import Me from './components/User/Me'
+import Users from './components/User/Users';
+import Main from './components/Main'
+import QuestionDetailBox from './components/Questions/QuestionDetailBox'
+import {SearchResultUser, SearchResultTags, SearchResultKwds} from './components/SearchResult/SearchResults'
+import Header, { Header2 } from './components/Banner/Header';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div>
+<<<<<<< Updated upstream
       Hello World
+=======
+    <Router>
+      <Header/>
+      <Switch>
+      <Route exact path="/" component={Main}/>
+      <Route exact path="/question/:question_id" component={QuestionDetailBox}/>
+      <Route exact path="/question/user/:user_id" component={SearchResultUser}/>
+      <Route exact path="/question/tagged" component={SearchResultTags}/>
+      <Route exact path="/question/kwds" component={SearchResultKwds}/>
+      <Route path="/users/me" component={Me}/>
+      <Route path="/history" component={History}/>
+      <Route exact path="/users" component={Users}/>
+      </Switch>
+    </Router>
+>>>>>>> Stashed changes
     </div>
   );
 }
--- src/components/Banner/Header.js
+++ src/components/Banner/Header.js
@@ -1,29 +1,52 @@
 import React, {useState} from 'react'
 import {getQuestionbyKwds} from '../../axios'
 import {useHistory} from 'react-router-dom'
-const Header = () => {
+import './image.css'
+import logo from '../../logo.png'
+
+import styles from "./Header.module.scss";
+import Button from '../Button';
+
+export const Header = () => {
+    const [user, setUser] = useState({ 
+        "id": 0,
+        "username": "waffle",
+        "created_at": 0,
+        "updated_at": 0,
+        "email": "waffle@wafflestudio.com",
+        "last_login": 0,
+        "nickname": "waffl-e",
+        "picture": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Waffles_with_Strawberries.jpg/1280px-Waffles_with_Strawberries.jpg",
+        "reputation": 0,
+        "question_count": 1,
+        "answer_count":2,
+        "bookmark_count":3
+      });
+
     let history = useHistory();
     const [command, setCommand] = useState('');
     const search = () => {
     /*GET /question/search/keywords*/
         history.push("/users/me")
     }
-    return (
-        <div>
-            <a /*TODO: provide href*/>wafflow</a>
-        <form role="search" action="/search">
-            <input onChange={(e)=>{setCommand(e.target.value)}} name="q" type="text" value={command} maxLength="200" placeholder="Search..."/>
-            <button onClick={()=>{console.log("click");search();}}>Search</button>
-        </form>
-        <div>
-            {/*user image redirected to user page*/}
-            
-            {/*reputation*/}
-
-            {/*history*/}
-
+  return (
+      
+    <div className={styles.header}>
+      <a href="/"><img alt="logo" className={styles.logo} src={logo} /></a>
+      <form className={styles.inputWithButton}  name="search-form" role="search" action="/questions" method="get">
+        <input className={styles.input} onChange={(e)=>{setCommand(e.target.value)}} name="q" type="text" value={command} maxLength="200" placeholder="Search your..."/>
+        <Button title="WAFFLE!"></Button>
+      </form>
+      <div className={styles.rightNav}>
+        <div className={styles.menus}>
+          <img className="profile-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Waffles_with_Strawberries.jpg/1280px-Waffles_with_Strawberries.jpg" alt="user"/>
+          <p className={styles.menuItem}>코린이</p>
+          <p className={styles.menuItem}>100</p>
         </div>
-        </div>
-    )
-}
-export default Header
+        <Button title="Profile"></Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
