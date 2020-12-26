977b502a61b290897e8fb61cf057a42db24653b9
gitiss <36957738+gitiss@users.noreply.github.com>
2020-12-26 12:57:22 +0900

    update user components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import History from './components/User/History'
+import Me from './components/User/Me'
+import Users from './components/User/Users';
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/users/me" component={Me}/>
+        <Route path="/history" component={History}/>
+        <Route exact path="/users" component={Users}/>
+      </Switch>
+    </Router>
   );
 }
 
