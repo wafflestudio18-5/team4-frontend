9e0d34a9a7bde184066a33b10ebeee379452fb79
David Kim <david15730@snu.ac.kr>
2020-12-26 13:52:25 +0900

    align syntax

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,21 +5,21 @@ import {Link} from 'react-router-dom'
 const Main = () => {
     return(
         <Fragment> 
-            <h1 className="main-title-box" style={{'text-Align':'center'}}>
+            <h1 className="main-title-box" style={{'text-align':'center'}}>
                 Wafflow는 와플스튜디오의 핵심적인 아카이브 서비스입니다. 
             </h1>
-            <h3 className="main-subtitle" style={{'text-Align':'center'}}>
+            <h3 className="main-subtitle" style={{'text-align':'center'}}>
                 풍부한 가능성, 신뢰할 수 있는 네트워크
             </h3>
-            <div style={{'text-Align':'center'}}>
+            <div style={{'text-align':'center'}}>
                  <img src="https://www.cuisinart.com/globalassets/recipes/recipe_6135_13606779" alt="waffle" align="center"/>
             </div>
             <br></br>
            
-            <h3 style={{'text-Align':'center'}}>
+            <h3 style={{'text-align':'center'}}>
                 지금 Wafflow와 함께히세요
             </h3> 
-            <div style={{'text-Align':'center'}}>
+            <div style={{'text-align':'center'}}>
                 <ul>
                     <li>
                         <Link to="/">to Main Page</Link>
